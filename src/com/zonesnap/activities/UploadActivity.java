10c27deb488d18d2f932ae3b0296ffad4efcc56a
Grant Spence <gcs278@vt.edu>
2013-11-07 22:55:11 -0500

    Set up framework for the activities

---
+++ src/com/zonesnap/activities/UploadActivity.java
@@ -0,0 +1,26 @@
+package com.zonesnap.activities;
+
+import com.zonesnap.zonesnap_app.R;
+import com.zonesnap.zonesnap_app.R.layout;
+import com.zonesnap.zonesnap_app.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class UploadActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_upload);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.upload, menu);
+		return true;
+	}
+
+}
